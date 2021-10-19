a66d1e11925e07ca73715dc6a9581a9d6733ce7d
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-10-18 22:44:27 -0400

    working on Fund Me project

--- Solidity-SmartContract-Blockchain/FundMe.sol
+++ Solidity-SmartContract-Blockchain/FundMe.sol
@@ -2,16 +2,28 @@
 
 pragma solidity >=0.6.6 <0.9.0;
 
-import "@chainlink/contracts/src/v0.8/interafaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
 
 contract FundMe {
     
     mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+    
+    // constructor gets called the moment your smart contract is deployed
+    constructor() public {
+        owner = msg.sender;
+    }
     
     function fund() public payable {
         // when you define a function as payable, you are telling the user that this function can be used for payments (ETH)
         // msg.sender & msg.value are keywords that can be used in any contract transaction.
+        
+        uint256 minimumUSD = 50 * 10 ** 18;
+        require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
         
         // what is the ETH -> USD conversion rate is?
         // This is where a oracle is needed to get off-chain data. The oracle is also responsible for validating it.
@@ -31,6 +43,30 @@ contract FundMe {
         // A tuple is a list of objects of potentially different types whose number is a constant at compile-time.
         
         (,int256 answer,,,) = priceFeed.latestRoundData();
-        return uint256(answer * 1000000000);
+        return uint256(answer * 10000000000);
+    }
+    
+    function getConversionRate(uint256  _ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * _ethAmount) / 1000000000000000000;
+        return ethAmountInUsd;
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    
+    function withdraw() payable onlyOwner public {
+        msg.sender.transfer(address(this).balance);
+        for (uint256 funderIndex=0; funderIndex<funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        
     }
 }
+
+
+
