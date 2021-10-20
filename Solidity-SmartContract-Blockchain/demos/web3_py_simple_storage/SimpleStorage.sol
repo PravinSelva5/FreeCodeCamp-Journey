833c93516e40cd544274e9439c2927f6ccfb22e8
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-10-19 22:38:57 -0400

    learning about python's web3 library

---
+++ Solidity-SmartContract-Blockchain/demos/web3_py_simple_storage/SimpleStorage.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+
+contract SimpleStorage {
+    uint256 public favouriteNumber;
+    bool favouriteBool;
+    
+    
+    struct People {
+        uint256 favouriteNumber;
+        string name;
+    }
+    
+    People[] public people;
+    
+    mapping(string => uint256) public nameToFavouriteNumber;
+    
+    People public person = People({favouriteNumber: 2, name: "Patrick"});
+    
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
+    
+    function retrieve() public view returns(uint256){
+        return favouriteNumber;
+    }
+    
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        people.push(People(_favouriteNumber, _name));
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+}
