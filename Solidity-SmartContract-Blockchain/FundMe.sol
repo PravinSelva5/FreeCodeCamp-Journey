57a7c188ca8edc1973dbf0fbcfa7a999fd07f6a3
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-02-21 22:34:32 -0500

    created a smart contract that allows users to fund smart contracts

--- Solidity-SmartContract-Blockchain/FundMe.sol
+++ Solidity-SmartContract-Blockchain/FundMe.sol
@@ -1,73 +1,65 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.6 <0.9.0;
+// Smart contract that lets anyone deposit ETH into the contract
+// Only the owner of the contract can withdraw the ETH
+pragma solidity >=0.6.6;
 
+// Get the latest ETH/USD price from chainlink price feed
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
 
 contract FundMe {
-    
+    using SafeMathChainlink for uint256;
+
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
     address public owner;
-    
-    // constructor gets called the moment your smart contract is deployed
+
+    // constructor gets called the instance the smart contract is deployed
     constructor() public {
         owner = msg.sender;
     }
-    
+
     function fund() public payable {
-        // when you define a function as payable, you are telling the user that this function can be used for payments (ETH)
-        // msg.sender & msg.value are keywords that can be used in any contract transaction.
-        
         uint256 minimumUSD = 50 * 10 ** 18;
         require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
-        
-        // what is the ETH -> USD conversion rate is?
-        // This is where a oracle is needed to get off-chain data. The oracle is also responsible for validating it.
-        // Remember interfaces don't have full function implementations
-        // Solidity doesn't know how to interact with a contract natively, as a developer, we need to state which functions can be called.
-        // Interfaces compile down to an ABI, tells solidity and other programming languages how it can interact with another contract.
-        //  - Therefore, when trying to interact with another smart contract, you will need its ABI.
     }
-        
     function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331); 
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         return priceFeed.version();
     }
-    
-    function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
-        // A tuple is a list of objects of potentially different types whose number is a constant at compile-time.
-        
+
+    function getPrice() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         (,int256 answer,,,) = priceFeed.latestRoundData();
+
         return uint256(answer * 10000000000);
     }
-    
-    function getConversionRate(uint256  _ethAmount) public view returns (uint256) {
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
         uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * _ethAmount) / 1000000000000000000;
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
         return ethAmountInUsd;
     }
-    
+
     modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
-    
-    
     function withdraw() payable onlyOwner public {
-        msg.sender.transfer(address(this).balance);
-        for (uint256 funderIndex=0; funderIndex<funders.length; funderIndex++){
+        msg.sender.transfer(address(this).balance); // this is a keyword in solidity, it refers to the smart contract you are currently using.
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
-        
     }
 }
 
-
-
+// Anytime you want to interact with an already deployed smart contrct you need an ABI. 
+// It allows you to know how it can interact with another contract.
+// Modifier
+// A modifier is used to change the behaviour of a function in a declarative way.
