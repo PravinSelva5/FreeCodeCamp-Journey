f11a4ef431726b1bb3610205ca9f49fab13c56ac
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-03-03 22:32:31 -0500

    learning how to deploy, test, and automate with brownie

---
+++ Solidity-SmartContract-Blockchain/web3_py_simple_storage/brownie_simple_storage/scripts/deploy.py
@@ -0,0 +1,15 @@
+from brownie import accounts
+import os
+
+
+def deploy_simple_storage():
+    # account = accounts[0]
+    # print(account)
+    # account = accounts.load("freecodecamp-development-account")
+    # print(account)
+    account = accounts.add(os.getenv("PRIVATE_KEY"))
+    print(account)
+
+
+def main():
+    deploy_simple_storage()
