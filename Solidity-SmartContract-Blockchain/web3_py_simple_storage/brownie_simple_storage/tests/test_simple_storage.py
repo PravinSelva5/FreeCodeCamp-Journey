582e3877757c7aa80fb23daf0133fae7efb324d3
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-03-05 21:58:20 -0500

    learned to use brownie to deploy and test

---
+++ Solidity-SmartContract-Blockchain/web3_py_simple_storage/brownie_simple_storage/tests/test_simple_storage.py
@@ -0,0 +1,28 @@
+from brownie import SimpleStorage, accounts
+
+
+def test_deploy():
+    # Arrange
+    account = accounts[0]
+    # Act
+    simple_storage = SimpleStorage.deploy({"from": account})
+    starting_value = simple_storage.retrieve()
+    expected = 0
+    # Assert
+    assert starting_value == expected
+
+
+def test_updating_storage():
+    # Arrange
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    # Act
+    expected = 15
+    simple_storage.store(expected, {"from": account})
+    # Assert
+    assert expected == simple_storage.retrieve()
+
+### The brownie-test feature is based of the python test library. Using their documentation will show you what methods you can use
+
+
+### Development networks are temporary, any scripts & transactions sent on these development blockchains are deleted after the script runs.
