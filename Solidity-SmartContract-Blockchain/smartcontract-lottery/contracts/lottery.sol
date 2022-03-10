89035e7914c18f0296d70c204974487a95fd245e
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-03-09 22:52:14 -0500

    working on a blockchain lottery smart contract

---
+++ Solidity-SmartContract-Blockchain/smartcontract-lottery/contracts/lottery.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.6.6;
+
+contract Lottery {
+    address payable[] public players;
+    uint256 public usdEntryFee;
+
+    constructor() public {
+        usdEntryFee = 50 * (10**18);
+    }
+
+    function enter() public payable {
+        require(condition);
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {}
+
+    function startLottery() public {}
+
+    function endLottery() public {}
+}
