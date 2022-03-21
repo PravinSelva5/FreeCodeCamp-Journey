9146cdd0054188b1fad9854f1d7f0cf309cf8fbd
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-03-21 19:24:04 -0400

    added integration scripts to test on rinkeby testnet

--- Solidity-SmartContract-Blockchain/smartcontract-lottery/scripts/helpful_scripts.py
+++ Solidity-SmartContract-Blockchain/smartcontract-lottery/scripts/helpful_scripts.py
@@ -2,10 +2,10 @@ from brownie import (
     accounts,
     network,
     config,
-    Contract,
+    MockV3Aggregator,
     VRFCoordinatorMock,
     LinkToken,
-    MockV3Aggregator,
+    Contract,
     interface,
 )
 
@@ -14,6 +14,9 @@ LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 
 def get_account(index=None, id=None):
+    # accounts[0]
+    # accounts.add("env")
+    # accounts.load("id")
     if index:
         return accounts[index]
     if id:
@@ -34,15 +37,14 @@ contract_to_mock = {
 
 
 def get_contract(contract_name):
-    """
-    This function will grab the contract addresses from the brownie config if defined, otherwise, it will deploy a mock verison of that contract, and return that mock contract.
-
-    Args:
-        contract_name (string)
-
-    Returns:
-        brownie.network.contract.ProjectContract: The most recently deployed version of the contract.
-
+    """This function will grab the contract addresses from the brownie config
+    if defined, otherwise, it will deploy a mock version of that contract, and
+    return that mock contract.
+        Args:
+            contract_name (string)
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently deployed
+            version of this contract.
     """
     contract_type = contract_to_mock[contract_name]
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
@@ -50,11 +52,15 @@ def get_contract(contract_name):
             # MockV3Aggregator.length
             deploy_mocks()
         contract = contract_type[-1]
+        # MockV3Aggregator[-1]
     else:
         contract_address = config["networks"][network.show_active()][contract_name]
+        # address
+        # ABI
         contract = Contract.from_abi(
             contract_type._name, contract_address, contract_type.abi
         )
+        # MockV3Aggregator.abi
     return contract
 
 
@@ -66,19 +72,18 @@ def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
     account = get_account()
     MockV3Aggregator.deploy(decimals, initial_value, {"from": account})
     link_token = LinkToken.deploy({"from": account})
-    VRFCoordinatorMock.deploy(link_token, {"from": account})
+    VRFCoordinatorMock.deploy(link_token.address, {"from": account})
     print("Deployed!")
 
 
 def fund_with_link(
     contract_address, account=None, link_token=None, amount=100000000000000000
-):
+):  # 0.1 LINK
     account = account if account else get_account()
     link_token = link_token if link_token else get_contract("link_token")
     tx = link_token.transfer(contract_address, amount, {"from": account})
-    # This is another way to interact with contracts that already exist.
     # link_token_contract = interface.LinkTokenInterface(link_token.address)
     # tx = link_token_contract.transfer(contract_address, amount, {"from": account})
     tx.wait(1)
-    print("Fund Contract!")
+    print("Fund contract!")
     return tx
